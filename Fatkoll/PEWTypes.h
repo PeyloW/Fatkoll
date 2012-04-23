8908e8607344d986eeb9f28752ab9416c3cfdb75
Fredrik Olsson <peylow@gmail.com>
2012-04-23 19:57:35 +0200

    Updated header files.

--- Fatkoll/PEWTypes.h
+++ Fatkoll/PEWTypes.h
@@ -3,7 +3,7 @@
 //  Fatkoll
 //
 //  Created by Fredrik Olsson on 2012-04-23.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Fredrik Olsson. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
